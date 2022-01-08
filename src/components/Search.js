defaa144b6b407565e7cb83d2a1023729232d6af
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2022-01-07 18:34:57 -0800

    added components folder and search.js with basic code.

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const SearchBar = (props) => {
+
+    return (
+        <View>
+            <TextInput placeholder='Search' />
+        </View>
+    )
+}
+
+export default SearchBar
