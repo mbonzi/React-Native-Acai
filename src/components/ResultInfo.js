dc180e905da95093fb06ba486107f886524484b3
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2022-01-07 18:46:49 -0800

    added resultinfo.js file with base code for search results

---
+++ src/components/ResultInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Image, Text } from 'react-native';
+
+const Info = () => {
+    return (
+        <View>
+            <Image />
+            <Text>
+                Main Text
+            </Text>
+            <Text>
+                Secondary information text
+            </Text>
+        </View>
+    )
+}
+
+export default Info;
